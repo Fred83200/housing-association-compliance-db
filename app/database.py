5153e0a49404a2f55fb749060517e01ce71c00c9
Amy Breslin <amy.breslin@solirius.com>
2026-05-08 14:28:42 +0100

    db back

---
+++ app/database.py
@@ -0,0 +1,27 @@
+import os
+from contextlib import contextmanager
+import psycopg2
+from dotenv import load_dotenv
+from psycopg2.extras import RealDictCursor
+
+load_dotenv()
+
+
+def get_connection():
+    return psycopg2.connect(
+        dbname=os.getenv("DATABASE_NAME", "housing_compliance_demo"),
+        user=os.getenv("DATABASE_USER", "fred"),
+        host=os.getenv("DATABASE_HOST", "localhost"),
+        port=os.getenv("DATABASE_PORT", "5432"),
+        cursor_factory=RealDictCursor,
+    )
+
+
+@contextmanager
+def db_cursor():
+    connection = get_connection()
+    try:
+        with connection.cursor() as cursor:
+            yield cursor
+    finally:
+        connection.close()
